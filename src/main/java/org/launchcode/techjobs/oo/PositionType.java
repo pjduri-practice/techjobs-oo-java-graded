9da8fc15c938c28eca9dd9719d9dd658ccb2f860
Your Name <pjduri@gmail.com>
2022-09-29 16:24:07 -0500

    All tests pass.  Uncommented code in Main.

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -12,40 +12,4 @@ public class PositionType extends JobField {
         super(aValue);
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-//    public String toString() {
-//        return value;
-//    }
-//
-//    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-//    //  their id fields match.
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        PositionType that = (PositionType) o;
-//        return getId() == that.getId();
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getId());
-//    }
-
-
-    // Getters and Setters:
-
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
-
 }
